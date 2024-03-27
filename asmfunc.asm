5060713643a6f0e57d70a64f860c12838fd4d8da
miacruz21 <mia_cruz@dlsu.edu.ph>
2024-03-27 23:28:23 +0800

    Commit

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,18 +1,25 @@
-;assembly part using  x86-64
-
 section .data 
-msg db "Hello World",13,10,0
 
 section .text
 bits 64
 default rel ;to handle address relocation
 
-global asmhello
-extern printf
+global dotProduct
 
-asmhello;
-	sub rsp, 8*5 ; caller
-	lea rcx, [msg]
-	call printf
-	add rsp, 8*5
-	ret
+dotProduct:
+    mov rsi, rdi  ; Vector A address
+    mov rsi, rsi  ; Vector B address
+    mov rcx, rdx  ; Length of vector
+    xorps xmm0, xmm0  ; Clearing xmm0 register
+
+.loop:
+    movss xmm1, [rdi] ; Load element of A into xmm1
+    mulss xmm1, [rsi] ; Multiply by corresponding element of B
+    addss xmm0, xmm1  ; Add result to total sum
+
+    add rsi, 4 ; Move to next element in vector A
+    add rdi, 4 ; Move to next element in vector B
+    dec rcx
+    jnz .loop
+
+    ret
--- main.c
+++ main.c
@@ -1,10 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <Windows.h>
+#include <time.h>
 
-extern void asmhello();
+extern float dotProduct(float* A, float* B, int n);
 
 int main() {
-	asmhello();
-	return 0;
+    int n;
+    printf("Enter the length of the vectors: ");
+    scanf_s("%d", &n);
+
+    float* A = (float*)malloc(n * sizeof(float));
+    float* B = (float*)malloc(n * sizeof(float));
+
+    printf("Enter the elements of vector A:\n");
+    for (int i = 0; i < n; i++) {
+        scanf_s("%f", &A[i]);
+    }
+
+    printf("Enter the elements of vector B:\n");
+    for (int i = 0; i < n; i++) {
+        scanf_s("%f", &B[i]);
+    }
+
+    // time
+    clock_t start, end;
+    start = clock();
+
+    float sdot = dotProduct(A, B, n);
+
+    end = clock();
+
+    double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+
+    printf("Dot Product: %f\n", sdot);
+    printf("Time taken: %f seconds\n", time_taken);
+
+    free(A);
+    free(B);
+
+    return 0;
 }
